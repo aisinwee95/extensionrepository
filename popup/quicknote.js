e55de0d7bf3b4594a5a51c67ade0849c9d661712
flomaxanne <florence_maxie@hotmail.com>
2017-11-05 18:39:50 +0800

    Files commited

---
+++ popup/quicknote.js
@@ -0,0 +1,271 @@
+/* initialise variables */
+
+
+var inputTitle = document.querySelector('.new-note input');
+var inputBody = document.querySelector('.new-note textarea');
+var inputOption = document.querySelector('.new-note select');
+var inputDate = document.querySelector('.new-note input[name="tododate"]');
+var noteContainer = document.querySelector('.note-container');
+
+
+var clearBtn = document.querySelector('.clear');
+var addBtn = document.querySelector('.add');
+
+/*  add event listeners to buttons */
+
+addBtn.addEventListener('click', addNote);
+clearBtn.addEventListener('click', clearAll);
+
+/* generic error handler */
+function onError(error) {
+  console.log(error);
+}
+
+/* display previously-saved stored notes on startup */
+
+initialize();
+
+function initialize() {
+  var gettingAllStorageItems = browser.storage.local.get(null);
+  gettingAllStorageItems.then((results) => {
+    var noteKeys = Object.keys(results);
+    for (let noteKey of noteKeys) {
+      var curValue = results[noteKey];
+      displayNote(noteKey,curValue);
+    }
+  }, onError);
+}
+
+/* Add a note to the display, and storage */
+
+function addNote() {
+  var noteTitle = inputTitle.value;
+  var noteBody = inputBody.value;
+  var noteDate = inputDate.value;
+  var noteOption = inputOption.value;
+  var gettingItem = browser.storage.local.get(noteTitle);
+  gettingItem.then((result) => {
+    var objTest = Object.keys(result);
+    if(objTest.length < 1 && noteTitle !== '' && noteBody !== '' && noteDate !== '' && noteOption !== '') {
+      inputTitle.value = '';
+      inputBody.value = '';
+	  inputDate.value = '';
+	  inputOption.value = '';
+      storeNote(noteTitle,noteBody,noteDate,noteOption);
+    }
+  }, onError);
+}
+
+/* function to store a new note in storage */
+
+function storeNote(title, body, date, options) {
+  var storingNote = browser.storage.local.set({ [title] : body});
+  storingNote.then(() => {
+    displayNote(title,body,date,options);
+  }, onError);
+}
+
+/* function to display a note in the note box */
+
+function displayNote(title, body, date, options) {
+
+  /* create note display box */
+  var note = document.createElement('div');
+  var noteDisplay = document.createElement('div');
+  var noteH = document.createElement('h2');
+  var notePara = document.createElement('p');
+  var notePara2 = document.createElement('p');
+  var notePara3 = document.createElement('p');
+  var deleteBtn = document.createElement('button');
+  var clearFix = document.createElement('div');
+
+  note.setAttribute('class','note');
+
+  noteH.textContent = title;
+  notePara.textContent = body;
+  notePara2.textContent = date;
+  notePara3.textContent = options;
+  deleteBtn.setAttribute('class','delete');
+  deleteBtn.textContent = 'Delete note';
+  clearFix.setAttribute('class','clearfix');
+
+  noteDisplay.appendChild(noteH);
+  noteDisplay.appendChild(notePara);
+  noteDisplay.appendChild(notePara2);
+  noteDisplay.appendChild(notePara3);
+  noteDisplay.appendChild(deleteBtn);
+  noteDisplay.appendChild(clearFix);
+
+  note.appendChild(noteDisplay);
+
+  /* set up listener for the delete functionality */
+
+  deleteBtn.addEventListener('click',(e) => {
+    const evtTgt = e.target;
+    evtTgt.parentNode.parentNode.parentNode.removeChild(evtTgt.parentNode.parentNode);
+    browser.storage.local.remove(title);
+  })
+
+  /* create note edit box */
+  var noteEdit = document.createElement('div');
+  var noteTitleEdit = document.createElement('input');
+  var noteBodyEdit = document.createElement('textarea');
+  var clearFix2 = document.createElement('div');
+
+  var updateBtn = document.createElement('button');
+  var cancelBtn = document.createElement('button');
+
+  updateBtn.setAttribute('class','update');
+  updateBtn.textContent = 'Update note';
+  cancelBtn.setAttribute('class','cancel');
+  cancelBtn.textContent = 'Cancel update';
+
+  noteEdit.appendChild(noteTitleEdit);
+  noteTitleEdit.value = title;
+  noteEdit.appendChild(noteBodyEdit);
+  noteBodyEdit.textContent = body;
+  noteEdit.appendChild(updateBtn);
+  noteEdit.appendChild(cancelBtn);
+
+  noteEdit.appendChild(clearFix2);
+  clearFix2.setAttribute('class','clearfix');
+
+  note.appendChild(noteEdit);
+
+  noteContainer.appendChild(note);
+  noteEdit.style.display = 'none';
+
+  /* set up listeners for the update functionality */
+
+  noteH.addEventListener('click',() => {
+    noteDisplay.style.display = 'none';
+    noteEdit.style.display = 'block';
+  })
+
+  notePara.addEventListener('click',() => {
+    noteDisplay.style.display = 'none';
+    noteEdit.style.display = 'block';
+  }) 
+
+  cancelBtn.addEventListener('click',() => {
+    noteDisplay.style.display = 'block';
+    noteEdit.style.display = 'none';
+    noteTitleEdit.value = title;
+    noteBodyEdit.value = body;
+  })
+
+  updateBtn.addEventListener('click',() => {
+    if(noteTitleEdit.value !== title || noteBodyEdit.value !== body) {
+      updateNote(title,noteTitleEdit.value,noteBodyEdit.value);
+      note.parentNode.removeChild(note);
+    } 
+  });
+}
+
+
+/* function to update notes */
+
+function updateNote(delNote,newTitle,newBody) {
+  var storingNote = browser.storage.local.set({ [newTitle] : newBody });
+  storingNote.then(() => {
+    if(delNote !== newTitle) {
+      var removingNote = browser.storage.local.remove(delNote);
+      removingNote.then(() => {
+        displayNote(newTitle, newBody);
+      }, onError);
+    } else {
+      displayNote(newTitle, newBody);
+    }
+  }, onError);
+}
+
+/* Clear all notes from the display/storage */
+
+function clearAll() {
+  while (noteContainer.firstChild) {
+      noteContainer.removeChild(noteContainer.firstChild);
+  }
+  browser.storage.local.clear();
+}
+
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var radius = canvas.height / 2;
+ctx.translate(radius, radius);
+radius = radius * 0.90
+setInterval(drawClock, 1000);
+
+function drawClock() {
+  drawFace(ctx, radius);
+  drawNumbers(ctx, radius);
+  drawTime(ctx, radius);
+}
+
+function drawFace(ctx, radius) {
+  var grad;
+  ctx.beginPath();
+  ctx.arc(0, 0, radius, 0, 2*Math.PI);
+  ctx.fillStyle = 'white';
+  ctx.fill();
+  grad = ctx.createRadialGradient(0,0,radius*0.95, 0,0,radius*1.05);
+  grad.addColorStop(0, '#333');
+  grad.addColorStop(0.5, 'white');
+  grad.addColorStop(1, '#333');
+  ctx.strokeStyle = grad;
+  ctx.lineWidth = radius*0.1;
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.arc(0, 0, radius*0.1, 0, 2*Math.PI);
+  ctx.fillStyle = '#333';
+  ctx.fill();
+}
+
+function drawNumbers(ctx, radius) {
+  var ang;
+  var num;
+  ctx.font = radius*0.15 + "px arial";
+  ctx.textBaseline="middle";
+  ctx.textAlign="center";
+  for(num = 1; num < 13; num++){
+    ang = num * Math.PI / 6;
+    ctx.rotate(ang);
+    ctx.translate(0, -radius*0.85);
+    ctx.rotate(-ang);
+    ctx.fillText(num.toString(), 0, 0);
+    ctx.rotate(ang);
+    ctx.translate(0, radius*0.85);
+    ctx.rotate(-ang);
+  }
+}
+
+function drawTime(ctx, radius){
+    var now = new Date();
+    var hour = now.getHours();
+    var minute = now.getMinutes();
+    var second = now.getSeconds();
+    //hour
+    hour=hour%12;
+    hour=(hour*Math.PI/6)+
+    (minute*Math.PI/(6*60))+
+    (second*Math.PI/(360*60));
+    drawHand(ctx, hour, radius*0.5, radius*0.07);
+    //minute
+    minute=(minute*Math.PI/30)+(second*Math.PI/(30*60));
+    drawHand(ctx, minute, radius*0.8, radius*0.07);
+    // second
+    second=(second*Math.PI/30);
+    drawHand(ctx, second, radius*0.9, radius*0.02);
+}
+
+function drawHand(ctx, pos, length, width) {
+    ctx.beginPath();
+    ctx.lineWidth = width;
+    ctx.lineCap = "round";
+    ctx.moveTo(0,0);
+    ctx.rotate(pos);
+    ctx.lineTo(0, -length);
+    ctx.stroke();
+    ctx.rotate(-pos);
+}
+
+ 
