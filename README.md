2645c370f38c6bc9e7b2ab5972b8a67448bdf3c3
flomaxanne <florence_maxie@hotmail.com>
2017-11-21 19:52:22 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# Easy Note Web Extension
+
---
+++ popup/easynote.js
@@ -0,0 +1,287 @@
+/* initialise variables */
+
+
+var taskInput = document.getElementById("new-task");
+var addButton = document.getElementsByTagName("button")[0];
+var incompleteTasksHolder = document.getElementById("incomplete-tasks");
+var completedTasksHolder = document.getElementById("completed-tasks");
+
+//New Task List Item
+var createNewTaskElement = function (taskString) {
+    //Create List Item
+    var listItem = document.createElement("li");
+
+    //input (checkbox)
+    var checkBox = document.createElement("input"); // checkbox
+    //label
+    var label = document.createElement("label");
+    //input (text)
+    var editInput = document.createElement("input"); // text
+    //button.edit
+    var editButton = document.createElement("button");
+    //button.delete
+    var deleteButton = document.createElement("button");
+
+    //Each element needs modifying
+
+    checkBox.type = "checkbox";
+    editInput.type = "text";
+
+    editButton.innerText = "Edit";
+    editButton.className = "edit";
+    deleteButton.innerText = "Delete";
+    deleteButton.className = "delete";
+
+    label.innerText = taskString;
+
+
+    // each element needs appending
+    listItem.appendChild(checkBox);
+    listItem.appendChild(label);
+    listItem.appendChild(editInput);
+    listItem.appendChild(editButton);
+    listItem.appendChild(deleteButton);
+
+    return listItem;
+}
+
+/* generic error handler */
+function onError(error) {
+    console.log(error);
+}
+
+/* display previously-saved stored notes on startup */
+
+initialize();
+
+function initialize() {
+    var gettingAllStorageItems = browser.storage.local.get(null);
+    gettingAllStorageItems.then((results) => {
+        var noteKeys = Object.keys(results);
+        for (let noteKey of noteKeys) {
+            var curValue = results[noteKey];
+            displayNote(noteKey, curValue);
+        }
+    }, onError);
+}
+
+
+
+// Add a new task
+var addTask = function () {
+    console.log("Add task.");
+    //Create a new list item with the text from #new-task:
+    var listItem = createNewTaskElement(taskInput.value);
+    //Append listItem to incompleteTasksHolder
+    incompleteTasksHolder.appendChild(listItem);
+    bindTaskEvents(listItem, taskCompleted);
+    taskInput.value = "";
+    var gettingItem = browser.storage.local.get(noteTitle);
+    gettingItem.then((result) => {
+        var objTest = Object.keys(result);
+        if(objTest.length < 1 && listItem !== '') {      
+        storeNote(listItem);
+       
+            
+        }
+    }, onError);
+}
+
+
+
+
+
+/* function to store a new note in storage */
+
+function storeNote(listItem) {
+    var storingNote = browser.storage.local.set({ listItem});
+    storingNote.then(() => {
+        bindTaskEvents(listItem);
+    }, onError);
+}
+
+
+
+// Edit an existing task
+var editTask = function () {
+    console.log("Edit Task...");
+
+    var listItem = this.parentNode;
+
+    var editInput = listItem.querySelector("input[type=text]")
+    var label = listItem.querySelector("label");
+
+    var containsClass = listItem.classList.contains("editMode");
+    //if the class of the parent is .editMode 
+    if (containsClass) {
+        //switch from .editMode 
+        //Make label text become the input's value
+        label.innerText = editInput.value;
+    } else {
+        //Switch to .editMode
+        //input value becomes the label's text
+        editInput.value = label.innerText;
+    }
+
+    // Toggle .editMode on the parent
+    listItem.classList.toggle("editMode");
+
+}
+
+
+// Delete an existing task
+var deleteTask = function () {
+    console.log("Delete task...");
+    var listItem = this.parentNode;
+    var ul = listItem.parentNode;
+
+    //Remove the parent list item from the ul
+    ul.removeChild(listItem);
+}
+
+// Mark a task as complete 
+var taskCompleted = function () {
+    console.log("Task complete...");
+    //Append the task list item to the #completed-tasks
+    var listItem = this.parentNode;
+    completedTasksHolder.appendChild(listItem);
+    bindTaskEvents(listItem, taskIncomplete);
+}
+
+// Mark a task as incomplete
+var taskIncomplete = function () {
+    console.log("Task Incomplete...");
+    // When checkbox is unchecked
+    // Append the task list item #incomplete-tasks
+    var listItem = this.parentNode;
+    incompleteTasksHolder.appendChild(listItem);
+    bindTaskEvents(listItem, taskCompleted);
+}
+
+var bindTaskEvents = function (taskListItem, checkBoxEventHandler) {
+    console.log("Bind list item events");
+    //select taskListItem's children
+    var checkBox = taskListItem.querySelector("input[type=checkbox]");
+    var editButton = taskListItem.querySelector("button.edit");
+    var deleteButton = taskListItem.querySelector("button.delete");
+
+    //bind editTask to edit button
+    editButton.onclick = editTask;
+
+    //bind deleteTask to delete button
+    deleteButton.onclick = deleteTask;
+
+    //bind checkBoxEventHandler to checkbox
+    checkBox.onchange = checkBoxEventHandler;
+}
+
+var ajaxRequest = function () {
+    console.log("AJAX Request");
+}
+
+// Set the click handler to the addTask function
+//addButton.onclick = addTask;
+addButton.addEventListener("click", addTask);
+addButton.addEventListener("click", ajaxRequest);
+
+
+// Cycle over the incompleteTaskHolder ul list items
+for (var i = 0; i < incompleteTasksHolder.children.length; i++) {
+    // bind events to list item's children (taskCompleted)
+    bindTaskEvents(incompleteTasksHolder.children[i], taskCompleted);
+}
+// Cycle over the completeTaskHolder ul list items
+for (var i = 0; i < completedTasksHolder.children.length; i++) {
+    // bind events to list item's children (taskIncompleted)
+    bindTaskEvents(completedTasksHolder.children[i], taskIncomplete);
+
+}
+
+
+
+function clearAll() {
+    while (noteContainer.firstChild) {
+        noteContainer.removeChild(noteContainer.firstChild);
+    }
+    browser.storage.local.clear();
+}
+
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var radius = canvas.height / 2;
+ctx.translate(radius, radius);
+radius = radius * 0.90
+setInterval(drawClock, 1000);
+
+function drawClock() {
+    drawFace(ctx, radius);
+    drawNumbers(ctx, radius);
+    drawTime(ctx, radius);
+}
+
+function drawFace(ctx, radius) {
+    var grad;
+    ctx.beginPath();
+    ctx.arc(0, 0, radius, 0, 2 * Math.PI);
+    ctx.fillStyle = 'white';
+    ctx.fill();
+    grad = ctx.createRadialGradient(0, 0, radius * 0.95, 0, 0, radius * 1.05);
+    grad.addColorStop(0, '#333');
+    grad.addColorStop(0.5, 'white');
+    grad.addColorStop(1, '#333');
+    ctx.strokeStyle = grad;
+    ctx.lineWidth = radius * 0.1;
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.arc(0, 0, radius * 0.1, 0, 2 * Math.PI);
+    ctx.fillStyle = '#333';
+    ctx.fill();
+}
+
+function drawNumbers(ctx, radius) {
+    var ang;
+    var num;
+    ctx.font = radius * 0.15 + "px arial";
+    ctx.textBaseline = "middle";
+    ctx.textAlign = "center";
+    for (num = 1; num < 13; num++) {
+        ang = num * Math.PI / 6;
+        ctx.rotate(ang);
+        ctx.translate(0, -radius * 0.85);
+        ctx.rotate(-ang);
+        ctx.fillText(num.toString(), 0, 0);
+        ctx.rotate(ang);
+        ctx.translate(0, radius * 0.85);
+        ctx.rotate(-ang);
+    }
+}
+
+function drawTime(ctx, radius) {
+    var now = new Date();
+    var hour = now.getHours();
+    var minute = now.getMinutes();
+    var second = now.getSeconds();
+    //hour
+    hour = hour % 12;
+    hour = (hour * Math.PI / 6) +
+    (minute * Math.PI / (6 * 60)) +
+    (second * Math.PI / (360 * 60));
+    drawHand(ctx, hour, radius * 0.5, radius * 0.07);
+    //minute
+    minute = (minute * Math.PI / 30) + (second * Math.PI / (30 * 60));
+    drawHand(ctx, minute, radius * 0.8, radius * 0.07);
+    // second
+    second = (second * Math.PI / 30);
+    drawHand(ctx, second, radius * 0.9, radius * 0.02);
+}
+
+function drawHand(ctx, pos, length, width) {
+    ctx.beginPath();
+    ctx.lineWidth = width;
+    ctx.lineCap = "round";
+    ctx.moveTo(0, 0);
+    ctx.rotate(pos);
+    ctx.lineTo(0, -length);
+    ctx.stroke();
+    ctx.rotate(-pos);
+}
